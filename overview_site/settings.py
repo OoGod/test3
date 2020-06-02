03695b7b61670924a4d75d929a99b4dafe5d688c
OoGod <1617605689@qq.com>
2020-06-02 21:11:01 +0800

    update

--- overview_site/settings.py
+++ overview_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django-heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -125,3 +127,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "staticfiles"),
 ]
+
+django_heroku.settings(locals())
